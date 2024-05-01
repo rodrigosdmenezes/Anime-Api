dffa872a0876037d51b8ff616cb727e6a24a46de
rodrigosdmenezes <rodrigosdmenzes@gmail.com>
2024-05-01 18:36:41 -0300

    Renomeado pastas

--- src/Data/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class AppDbContext : DbContext
-{
-    public static string BaseDirectory { get; internal set; }
-    public DbSet<Anime> Animes { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite("Data Source=animes.db");
-}
